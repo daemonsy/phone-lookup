df768ac15d052d2c5f6ca24ac7c20da040a1b55c
Damon Aw <daemonsy@gmail.com>
2016-10-21 23:39:21 -0400

    Use Throng to get a cluster

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import throng from "throng";
 import Schema from "./graphql/schema.js";
 
 const port = process.env.PORT || 4000;
+const workers = process.env.WEB_CONCURRENCY || 1;
 const app = express();
 
 app.use('/graph', graphqlHTTP({
@@ -10,6 +12,11 @@ app.use('/graph', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(port, (error, results) => {
-  console.log(`Ready for requests on port ${port}`);
-});
+const start = (id) => {
+  app.listen(port, (error, results) => {
+    console.log(`Ready for requests for worker ${id} on port ${port}`);
+  });
+};
+
+throng(workers, start);
+
